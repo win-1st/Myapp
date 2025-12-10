0c7b2181a8b44d22de7384f281abad384c5eea96
thang <thang.0398173863@gmail.com>
2025-12-10 15:49:24 +0700

    Update layout & index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="welcom" options={{ headerShown: false }} />
         <Stack.Screen name="signin" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
